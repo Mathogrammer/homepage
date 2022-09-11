71e1268aacfbe1667e40c59f8a4a605ac3da9714
Mathogrammer <38832422+Mathogrammer@users.noreply.github.com>
2022-09-11 15:52:25 +0200

    fix: seo entries

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,13 @@ import { i18n } from '@lingui/core';
 import { I18nProvider } from '@lingui/react';
 import { de, en } from 'make-plural/plurals';
 import { useRouter } from 'next/router';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useMemo } from 'react';
 import { ThemeProvider } from '../src/contexts/theme';
 import { messages } from '../src/locales/en/messages';
 import '../src/index.css';
 import 'flagpack/dist/flagpack.css';
 import { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 
 i18n.loadLocaleData({
     en: { plurals: en },
@@ -21,6 +22,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     const router = useRouter();
     const locale = router.locale || router.defaultLocale || 'de';
     const firstRender = useRef(true);
+    const canonicalUrl: string = useMemo(() => {
+        return (
+            `https://www.amantel.de` + (router.asPath === "/" ? "" : router.asPath)
+        ).split("?")[0];
+    }, [router]);
 
     if (pageProps.translation && firstRender.current) {
         //load the translations for the locale
@@ -44,6 +50,23 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider>
             <I18nProvider i18n={i18n}>
+                <DefaultSeo
+                    canonical={canonicalUrl}
+                    openGraph={{
+                        url: 'https://www.amantel.de',
+                        locale: 'de-DE',
+                        title: 'Alexander Mantel\'s Homepage',
+                        description: 'Open Graph Description',
+                        type: 'website',
+                        images: [{
+                            url: 'www.amantel.de/profile/profilePicRounded.png',
+                            secureUrl: 'https://www.amantel.de/profile/profilePicRounded.png',
+                            type: 'image/png',
+                            alt: 'Picture of Alexander Mantel\'s face in a circular frame.'
+                        }],
+                        site_name: 'Alexander Mantel\'s Homepage'
+                    }}
+                />
                 <Component {...pageProps} />
             </I18nProvider>
         </ThemeProvider>
