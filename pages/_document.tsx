6e30f801e78a293e3334eab8ef41c6bae1cee136
Alex M. <amm15@tu-clausthal.de>
2025-03-03 02:50:28 +0100

    fix: cross-origin value

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
                     href='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css'
                 />
                 <link rel='preconnect' href='https://fonts.googleapis.com' />
-                <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
+                <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin={'anonymous'} />
                 <link
                     href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap'
                     rel='stylesheet'
